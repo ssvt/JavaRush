c7837c2ca870ef7e45f8315618ea51b380ff647e
Inventor <sergey.svyatoha@gmail.com>
2020-05-12 21:02:41 +0300

    12.05 ресторан уровень 10

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/Advertisement.java
@@ -1,38 +1,45 @@
 package com.javarush.task.task27.task2712.ad;
 
-public class Advertisement
-{
-    private Object content;                             // видео
-    private String name;
-    private long initialAmount;                         //начальная сумма, стоимость рекламы в копейках.
-    private int hits;                                   // количество оплаченных показов
-    private int duration;                               //продолжительность в секундах
-    private long  amountPerOneDisplaying;
-    public Advertisement(Object content, String name, long initialAmount, int hits, int duration)
-    {
+public class Advertisement {
+    private Object content; //видео
+    private String name; //название
+    private long initialAmount; //начальная сумма, стоимость рекламы в копейках
+    private int hits; //количество оплаченных показов
+    private int duration; // продолжительность в секундах
+    private long amountPerOneDisplaying; //стоимость одного показа рекламного объявления в копейках
+
+    public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
         this.name = name;
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
-        this.amountPerOneDisplaying = initialAmount/hits;
+        this.amountPerOneDisplaying = (hits > 0) ? initialAmount / hits : 0;
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
-    public int getDuration() { return duration; }
+    public int getDuration() {
+        return duration;
+    }
 
-    public long getAmountPerOneDisplaying() { return amountPerOneDisplaying; }
+    public long getAmountPerOneDisplaying() {
+        return amountPerOneDisplaying;
+    }
 
-    public int getHits() { return hits;  }
+    public void revalidate() {
+        if (hits <= 0) throw new UnsupportedOperationException();
+        else hits--;
+    }
 
-    public void revalidate(){
+    @Override
+    public String toString() {
+        return name + " is displaying... " + getAmountPerOneDisplaying() + ", " + getAmountPerOneDisplaying()/duration*1000;
+    }
 
-        initialAmount -= amountPerOneDisplaying;
-        hits--;
-
-        if (hits <= 0){
-            throw new UnsupportedOperationException();
-        }
+    public int getHits() {
+        return hits;
     }
 }
--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/ad/AdvertisementManager.java
@@ -2,22 +2,84 @@ package com.javarush.task.task27.task2712.ad;
 
 import com.javarush.task.task27.task2712.ConsoleHelper;
 
-import java.io.IOException;
 import java.util.*;
 
 public class AdvertisementManager {
-    private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
-    private final int timeSeconds;
-
+    final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+    private int timeSeconds;
+//    private List<Advertisement> storageList = storage.list();
 
     public AdvertisementManager(int timeSeconds) {
         this.timeSeconds = timeSeconds;
     }
 
     public void processVideos(){
-        List<Advertisement> availableVideos = storage.list();
-        if (availableVideos.isEmpty()) { throw new NoVideoAvailableException();  }
+
+        List<Advertisement> storageList = storage.list();
+        List<Advertisement> resultList = getVideos(timeSeconds, storageList.size() - 1, storageList);
+
+        if (resultList.size() == 0) {
+            throw new NoVideoAvailableException();
+        }
+
+        Collections.sort(resultList, (ad1, ad2) -> {
+            int result = (int) (ad2.getAmountPerOneDisplaying() - ad1.getAmountPerOneDisplaying());
+            if (result == 0) {
+                result = (int) ((ad1.getAmountPerOneDisplaying() * 1000 / ad1.getDuration())
+                        - (ad2.getAmountPerOneDisplaying() * 100 / ad2.getDuration()));
+            }
+            return result;
+        });
+
+        resultList.forEach(advertisement -> {
+
+            advertisement.revalidate();
+            ConsoleHelper.writeMessage(String.format("%s is displaying... %d, %d", advertisement.getName()
+                    , advertisement.getAmountPerOneDisplaying()
+                    , advertisement.getAmountPerOneDisplaying()* 1000/advertisement.getDuration()));
 
 
+
+        });
+
+    }
+
+    private List<Advertisement> getVideos(int maxDuration, int adCount, List<Advertisement> storageList){
+
+        if (maxDuration <= 0 || adCount < 0) {
+            return new ArrayList<>();
+        }
+        Advertisement adElement = storageList.get(adCount);
+        List<Advertisement> withoutAddingAd = getVideos(maxDuration, adCount - 1, storageList);
+        if (adElement.getDuration() <= maxDuration){
+            List<Advertisement> withAddingAd = getVideos(maxDuration - adElement.getDuration()
+                    , adCount - 1, storageList);
+            withAddingAd.add(adElement);
+            return compareLists(withAddingAd, withoutAddingAd);
+        }else {
+            return withoutAddingAd;
+        }
+    }
+
+    private List<Advertisement> compareLists(List<Advertisement> withAddingAd, List<Advertisement> withoutAddingAd){
+        List<List<Advertisement>> lists = Arrays.asList(withAddingAd, withoutAddingAd);
+        return Collections.max(lists, (list1, list2) -> {
+            int result = (int) (getListProfit(list1) - getListProfit(list2));
+            if (result == 0) {
+                result = getListDuration(list1) - getListDuration(list2);
+            }
+            if (result == 0) {
+                result = list2.size() - list1.size();
+            }
+            return result;
+        });
+    }
+
+    private int getListDuration(List<Advertisement> advertisementList){
+        return advertisementList.stream().mapToInt(Advertisement::getDuration).sum();
+    }
+
+    private long getListProfit(List<Advertisement> advertisementList){
+        return advertisementList.stream().mapToLong(Advertisement::getAmountPerOneDisplaying).sum();
     }
 }
